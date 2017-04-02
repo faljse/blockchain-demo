78a84d29ca3d6f6fb7e1b36a8d42092c8d0400b7
jcohen98 <joshuacohen98@gmail.com>
2017-04-01 23:47:50 -0400

    Working socket

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var routes = require('./routes/index');
 
 var app = express();
@@ -52,6 +53,7 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
 
 
 module.exports = app;
